48a608763031684cc132d815375992a3fe208e87
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-08-09 21:22:22 +0200

    eto ga sve je namjesteno...samo trebam jos uradit branch-ove

---
+++ js/include.js
@@ -0,0 +1,10 @@
+$(window).on("load", function () {
+    $(document).ready(function () {
+        console.log('juhu')
+        /* $('#home').load('/views/home.html')
+        $('#skills').load('/views/skills.html')
+        $('#team').load('/views/teams.html')
+        $('#project').load('/views/project.html')
+        $('#footer').load('/views/footer.html') */
+    })
+})
