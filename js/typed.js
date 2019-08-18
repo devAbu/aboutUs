0f4276d3699a3cc970d0aa5089a8d1e4306cb2ce
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-08-18 18:37:57 +0200

    pixi i typed js ubaceni

---
+++ js/typed.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+    strings: ["This is a JavaScript library", "This is an ES6 module"],
+    smartBackspace: true,
+    loop: true,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500,
+    typeSpeed: 30,
+});
