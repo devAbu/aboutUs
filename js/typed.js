86eab39b4c555758111cd94128d434a8b602e513
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-08-18 20:40:57 +0200

    :D

--- js/typed.js
+++ js/typed.js
@@ -2,8 +2,9 @@ var typed = new Typed('#typed', {
     strings: ["This is a JavaScript library", "This is an ES6 module"],
     smartBackspace: true,
     loop: true,
-    fadeOut: true,
+    /* fadeOut: true,
     fadeOutClass: 'typed-fade-out',
-    fadeOutDelay: 500,
+    fadeOutDelay: 500, */
     typeSpeed: 30,
+    backSpeed: 30
 });
