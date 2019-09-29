1511062e47e0fbe687c6d2ab17bdb26019d763d6
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-09-29 14:45:14 +0200

    zavrsene ikone...teams popravljen
    ikone mozemo promijenit...ovo je za sad

--- js/swapImage.js
+++ js/swapImage.js
@@ -12,8 +12,10 @@ $(document).ready(function () {
 
                 progress = false
                 $(this).fadeOut(1000, function () {
+                    $('#slika').css('display', '')
                     $(this).attr('src', centerImage).bind('onreadystatechange load', function () {
                         if (this.complete) {
+                            $('#slika').css('display', 'none')
                             $(this).fadeIn(1000);
                         }
                     });
@@ -21,9 +23,11 @@ $(document).ready(function () {
                 });
                 centriranaSlika.fadeOut(1000, function () {
                     centriranaSlika.attr('src', clickedImage).bind('onreadystatechange load', function () {
+                        $('#slika').css('display', '')
                         if (this.complete) {
                             centriranaSlika.fadeIn(1000);
                             progress = true
+                            $('#slika').css('display', 'none')
                         }
                     });
                 })
