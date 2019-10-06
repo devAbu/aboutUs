d8ee5112d203d81c34c838b771ffb9943223d9cc
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-10-06 19:37:06 +0200

    popravljen loading malo (moze bit jos bolji)
    na pocetku samo bijela stranica i kasnije se pojavi loading

--- js/src/index.js
+++ js/src/index.js
@@ -1,4 +1,3 @@
-$('.loader').addClass('hidden')
 import {
     Black,
     White
--- js/src/shape.js
+++ js/src/shape.js
@@ -1,3 +1,4 @@
+$('.loader').addClass('hidden')
 import {
     EASE,
     PI2,
@@ -235,3 +236,25 @@ export class Polygon extends Base {
     }
 
 }
+
+//ZOOM
+$(document).ready(function () {
+    setTimeout(function () {
+        $('canvas').not('div.container > canvas').addClass('zoomCanvas');
+        $('body').append("<script>$('#skills').load('./views/skills.html')</script>")
+        $('body').append("<script>$('#teams').load('./views/teams.html')</script>")
+        $('body').append("<script>$('#project').load('./views/project.html')</script>")
+    }, 7000);
+
+    setTimeout(function () {
+        $('canvas').not('div.container > canvas').remove();
+        /* $('.welcome').css('background-color', 'white'); */
+        $('.welcome').remove();
+
+    }, 9500);
+    setTimeout(function () {
+        $('body').append('<script src="js/fullPage.js"></script>')
+        $('body').append("<script>$('#home').load('./views/home.html')</script>")
+        $('#fullPage').removeClass('hidden')
+    }, 9500)
+})
