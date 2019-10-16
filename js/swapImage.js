d893a298ffd1572e173310b59183f1624d79e43e
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-10-16 22:47:52 +0200

    team
    fali muhina i dizdina slika...treba poboljsat prebacivanje teksta

--- js/swapImage.js
+++ js/swapImage.js
@@ -5,8 +5,16 @@ $(document).ready(function () {
     $('.img-responsive').click(function (e) {
         if (progress) {
             if ($(this).attr("id") !== "slika3") {
+
+
+                $(".teamMore").removeClass("active");
+                $("#" + $(this).attr("alt")).addClass("active");
+
                 var clickedImage = $(this).attr("src")
                 var centerImage = $("#slika3").attr("src")
+
+                var idClickedImage = $(this).attr("alt")
+                var idCenterImage = $("#slika3").attr("alt")
 
                 var centriranaSlika = $('#slika3')
 
@@ -14,6 +22,7 @@ $(document).ready(function () {
                 $(this).fadeOut(1000, function () {
                     $('#slika').css('display', '')
                     $(this).attr('src', centerImage).bind('onreadystatechange load', function () {
+                        $(this).attr('alt', idCenterImage)
                         if (this.complete) {
                             $('#slika').css('display', 'none')
                             $(this).fadeIn(1000);
@@ -22,8 +31,9 @@ $(document).ready(function () {
 
                 });
                 centriranaSlika.fadeOut(1000, function () {
+                    $('#slika').css('display', '')
                     centriranaSlika.attr('src', clickedImage).bind('onreadystatechange load', function () {
-                        $('#slika').css('display', '')
+                        centriranaSlika.attr("alt", idClickedImage)
                         if (this.complete) {
                             centriranaSlika.fadeIn(1000);
                             progress = true
