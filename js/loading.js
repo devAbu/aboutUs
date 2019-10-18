4aef5fd6d2a12582567454a98fd02918612ac995
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-10-18 21:58:33 +0200

    ispravljene neke greske
    ima ih jos :cry:

--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,13 @@
+var width = window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+if (width <= 767) {
+    $('.s3').addClass('normalScroll')
+} else {
+    $('.s3').removeClass('normalScroll')
+}
+
 $(".welcome").addClass('hidden');
 
 $(window).on("load", function () {
